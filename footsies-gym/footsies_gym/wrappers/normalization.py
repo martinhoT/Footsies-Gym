49c42162120576777011d3b7e0b70f09f84cc65e
martinhoT <martinho.tavarex@gmail.com>
2023-11-03 14:54:42 +0000

    Wrapper for normalization of entire observation space, instead of just move frames

--- footsies-gym/footsies_gym/wrappers/normalization.py
+++ footsies-gym/footsies_gym/wrappers/normalization.py
@@ -3,24 +3,29 @@ from gymnasium import spaces
 from ..moves import FootsiesMove, footsies_move_index_to_move
 
 
-class FootsiesMoveFrameNormalized(gym.ObservationWrapper):
-    """Normalizes move frame durations to be between `0` and `1`, inclusive. `0` indicates the start of the move, while `1` indicates the end of it"""
+class FootsiesNormalized(gym.ObservationWrapper):
+    """Normalizes all observation space variables
+    
+    Move frame durations will be between `0` and `1`, inclusive. `0` indicates the start of the move, while `1` indicates the end of it
+    """
 
     def __init__(self, env):
         super().__init__(env)
         relevant_moves = set(FootsiesMove) - {FootsiesMove.WIN, FootsiesMove.DEAD}
         self.observation_space = spaces.Dict(
             {
-                "guard": spaces.MultiDiscrete([4, 4]),  # 0..3
+                "guard": spaces.Box(low=0.0, high=1.0, shape=(2,)),  # 0..3
                 "move": spaces.MultiDiscrete(
                     [len(relevant_moves), len(relevant_moves)]
                 ),
                 "move_frame": spaces.Box(low=0.0, high=1.0, shape=(2,)),
-                "position": spaces.Box(low=-4.4, high=4.4, shape=(2,)),
+                "position": spaces.Box(low=-1.0, high=1.0, shape=(2,)),
             }
         )
 
     def observation(self, obs):
+        obs["guard"] /= 3.0
+        obs["position"] /= 4.4
         obs["move_frame"][0] = (
             obs["move_frame"][0]
             / footsies_move_index_to_move[obs["move"][0]].value.duration
