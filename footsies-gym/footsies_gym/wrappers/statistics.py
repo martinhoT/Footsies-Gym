b2d0ada8c2c7b25889ee0c1d889113a2087da953
martinhoT <martinho.tavarex@gmail.com>
2023-11-19 16:24:50 +0000

    Added new training statistic and report method

--- footsies-gym/footsies_gym/wrappers/statistics.py
+++ footsies-gym/footsies_gym/wrappers/statistics.py
@@ -8,7 +8,9 @@ class FootsiesStatistics(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
         self._special_moves_per_episode = []
+        self._special_moves_from_neutral_per_episode = []
         self._special_moves_per_episode_counter = 0
+        self._special_moves_from_neutral_per_episode_counter = 0
         self._prev_p1_move = None  # used to make sure special moves are only counted when they are performed, and not every time step they are active
 
     def _get_p1_move(self, obs) -> FootsiesMove:
@@ -30,6 +32,13 @@ class FootsiesStatistics(gym.Wrapper):
             FootsiesMove.N_SPECIAL,
         }:
             self._special_moves_per_episode_counter += 1
+
+            if self._prev_p1_move not in {
+                FootsiesMove.B_ATTACK,
+                FootsiesMove.N_ATTACK,
+            }:
+                self._special_moves_from_neutral_per_episode += 1
+        
         self._prev_p1_move = p1_move
 
         if terminated or truncated:
@@ -43,3 +52,20 @@ class FootsiesStatistics(gym.Wrapper):
     @property
     def metric_special_moves_per_episode(self):
         return self._special_moves_per_episode
+
+    @property
+    def metric_special_moves_from_neutral_per_episode(self):
+        return self._special_moves_from_neutral_per_episode
+    
+    def report(self):
+        total_episodes = len(self.metric_special_moves_per_episode)
+        total_special_moves = sum(self.metric_special_moves_per_episode)
+        total_special_moves_from_neutral = sum(self.metric_special_moves_from_neutral_per_episode)
+
+        print("Report")
+        print(" Special moves")
+        print(f"  Average: {total_special_moves / total_episodes}")
+        print(f"  Total: {total_special_moves}")
+        print(" Special moves")
+        print(f"  Average: {total_special_moves_from_neutral / total_episodes}")
+        print(f"  Total: {total_special_moves_from_neutral}")
